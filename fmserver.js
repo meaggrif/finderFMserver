f938e2ed74ea6d438d8860d5ab1c292d3fa495e6
meaggrif <meag143@hotmail.com>
2019-12-02 17:44:33 -0500

    added server js file and json files

---
+++ fmserver.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+const app = express();
+const port = 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/sphi-rwax.json";
+  fetch(baseURL)
+    .then(r => r.json())
+    .then(data => {
+      console.log(data);
+      res.send({ data: data });
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/error");
+    });
+});
+
+app.post("/", (req, res) => {});
+
+app.put("/", (req, res) => {});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
