c93118be64a629517716cb89cd593b10f7e85cd5
meaggrif <meag143@hotmail.com>
2019-12-02 18:49:14 -0500

    updated server to enable cors

--- fmserver.js
+++ fmserver.js
@@ -4,8 +4,11 @@ const fetch = require("node-fetch");
 const app = express();
 const port = process.env.PORT || 8000;
 
+const cors = require("cors");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   const baseURL =
